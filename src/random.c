cf8039bedda340a982ffd35ccade3800f49b70a2
Гриша Тарасенко <fle4er@yandex.ru>
2016-04-11 22:28:07 +0600

    Напил интерфейса

--- src/random.c
+++ src/random.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-	FILE * data;
-	data = fopen("dic.txt", "r");
-	char n;
+	FILE *data = fopen("dic.txt", "r");
+	char n[20];
 	for (int i = 0; i < 4; i++)
         fscanf(data, "%s\n", &n);
 	fclose(data);
+    printf("%s\n", n);
 	return 0;
 }
