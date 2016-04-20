3170501c3dee848cf3e2620a1313e0f67b71d085
simplex1337 <cas98@bk.ru>
2016-04-20 19:25:59 +0600

    mda 2.0

--- src/random.c
+++ src/random.c
@@ -3,11 +3,15 @@
 int main()
 {
 	FILE * data; 
-	data = fopen("2.txt", "r");
-	char n[4];
+	data = fopen("dic.txt", "r");
+	int k = 1;
+	int i;
+	char n[10];
 	//for (int i = 0; i < 4; i++)
-		fscanf(data, "%s\n", &n);
+	fscanf(data, "%s\n", &n);
+	for (i = 6; i < 8; i++)
+		printf("%s\n", n[i]);
 	fclose(data);
-	printf("%s\n", n);
+	//printf("%s\n", n);
 	return 0;
 }
