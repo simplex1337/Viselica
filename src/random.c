61978be498baa004bd434d2e576f1ceb7b87b13a
simplex1337 <cas98@bk.ru>
2016-04-20 18:29:16 +0600

    polnoe

--- src/random.c
+++ src/random.c
@@ -3,10 +3,10 @@
 int main()
 {
 	FILE * data; 
-	data = fopen("dic.txt", "r");
-	char n[20];
-	for (int i = 0; i < 4; i++)
-		fscanf(data, "%s\n", &n[20]);
+	data = fopen("2.txt", "r");
+	char n[3];
+	//for (int i = 0; i < 4; i++)
+		fscanf(data, "%s\n", &n[3]);
 	fclose(data);
     printf("%s\n", n);
 	return 0;
