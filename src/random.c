ed7630fc8b95a297d5408a45e3bed1e4ec4b31be
simplex1337 <cas98@bk.ru>
2016-04-20 20:57:37 +0600

    пристрелите меня

--- src/random.c
+++ src/random.c
@@ -5,13 +5,14 @@ int main()
 	FILE * data; 
 	data = fopen("dic.txt", "r");
 	int k = 1;
-	int i;
+	int i, j;
 	char n[10];
 	//for (int i = 0; i < 4; i++)
-	fscanf(data, "%s\n", &n);
-	for (i = 6; i < 8; i++)
-		printf("%s\n", n[i]);
+	fscanf(data, "%s\n", &n);	
+	for (i = 0; !feof(data) != 1; i++)
+		fscanf(data, "%s\n", &n);
 	fclose(data);
+	printf("%d\n", j);
 	//printf("%s\n", n);
 	return 0;
 }
