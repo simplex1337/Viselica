e77221dd5343a65e37b5a3bf43eeccefbb001d75
Ksugont <ksugont@gmail.com>
2016-04-20 15:38:21 +0700

    Добавлена вторая версия функции ввода

--- src/input.c
+++ src/input.c
@@ -16,3 +16,25 @@ int main() {
 	}
 return 0;
 }
+
+// Гришина версия
+
+#include <curses.h>
+#include <locale.h>
+
+int main() {
+	setlocale( LC_ALL,"" );
+	initscr();
+	char symbol;
+	char button;
+	char a;
+	while (symbol !='\n') {
+	mvwprintw(stdscr, 0, 0, "Введите букву: %с", symbol);
+	symbol = getch();
+	if (button == KEY_BACKSPACE) {
+		symbol = '\0';
+	}
+	clear();
+}
+return 0;
+}
