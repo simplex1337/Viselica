af9cef2e657a5424a1dd55c9d0e42ff6623b0b43
simplex1337 <cas98@bk.ru>
2016-04-27 14:16:05 +0600

    yawer

--- src/random.c
+++ src/random.c
@@ -7,12 +7,9 @@ int main()
 	int k = 1;
 	int i, j;
 	char n[10];
-	//for (int i = 0; i < 4; i++)
-	fscanf(data, "%s\n", &n);	
-	for (i = 0; !feof(data) != 1; i++)
-		fscanf(data, "%s\n", &n);
+	while( !feof(data) )  
+		fscanf(data, "%s", &n ); 
 	fclose(data);
-	printf("%d\n", j);
-	//printf("%s\n", n);
+	printf("%s\n", n);
 	return 0;
 }
