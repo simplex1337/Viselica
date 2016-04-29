2dd7920291685d926c5ea9a55a9d4680a4f415cf
simplex1337 <cas98@bk.ru>
2016-04-29 19:32:13 +0600

    рабочий рандом

--- src/random.c
+++ src/random.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int getrand(int min, int max);
 
 int main()
 {
 	FILE *data = fopen("dic.txt", "r");
 	char *n;
-	int num;
-	scanf("%d", &num);
+	time_t t;
+	int num, min, max, tema;
+	srand((unsigned) time(&t));
+	scanf("%d", &tema);
+	//scanf("%d %d'", &min, &max);
+	if (tema == 1) {
+		min = 0;
+		max = 25;
+	}
+		if (tema == 2) {
+		min = 26;
+		max = 48;
+	}
+		if (tema == 3) {
+		min = 49;
+		max = 73;
+	}
+	num = getrand(min, max);
 	int i;
 	for(i = 0;  i < num; i++) {
 		fscanf(data, "%s\n", n);
@@ -14,4 +34,10 @@ int main()
 	printf("%s\n", n); 
 	fclose(data);
 	return 0;
+}
+
+int getrand(int min, int max)
+{
+	srand(rand() + clock());
+	return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
 }
