ec1ca491ee3c8098313cb7e9578c39e921dccbbf
Гриша Тарасенко <fle4er@yandex.ru>
2016-05-03 23:54:35 +0600

    Update asteriks.c

--- src/asteriks.c
+++ src/asteriks.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <wchar.h>
 
-extern char word[];
-int asteriks()
+extern wchar_t word[];
+void asteriks(char cens[], wchar_t guess)
 {
-    len = strlen(word);
-	return 0;
+    int i;
+    char guess_chr;
+    wcstombs(&guess_chr, &guess, 1);
+    for (i = 0; i < wcslen(word) * 2; i++) {
+        if (guess_chr == 'л') 
+            cens[i] = 'л';
+        else
+            cens[i] = '*';
+    }
+    cens[i+1] = '\0';
 }
