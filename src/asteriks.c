4c2239a3f1d51336b47960ed397ddecdb484a6fc
Гриша Тарасенко <fle4er@yandex.ru>
2016-05-04 20:14:09 +0600

    Попытка исправить wcslen

--- src/asteriks.c
+++ src/asteriks.c
@@ -9,11 +9,13 @@ void asteriks(char cens[], wchar_t guess)
     int i;
     char guess_chr;
     wcstombs(&guess_chr, &guess, 1);
-    for (i = 0; i < wcslen(word) * 2; i++) {
-        if (guess_chr == 'л') 
-            cens[i] = 'л';
-        else
-            cens[i] = '*';
+    int len;
+    if (wcslen(word) % 2 == 1)
+        len = wcslen(word) * 2 - 1;
+    else
+        len = wcslen(word) * 2;
+    for (i = 0; i < len; i++) {
+        cens[i] = '*';
     }
     cens[i+1] = '\0';
 }
--- src/random.c
+++ src/random.c
@@ -1,5 +1,4 @@
 //реализована функция выбора рандомного слова из словаря по трем темам
-#include "data"
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -8,6 +7,7 @@
 int num;
 void random_word(unsigned int choice)
 {
+    FILE *data = fopen("dic.txt", "r");
     extern wchar_t word[];
 	int min, max, i; // tema - номер темы, принимает значения 0, 1, 2
 	if (choice == 0) { // животные
@@ -24,8 +24,8 @@ void random_word(unsigned int choice)
 	}
 	srand(time(NULL));
 	num = rand() / (RAND_MAX + 1.0) * (max - min) + min;
-    for (i = 0, num; num > 0; i++)
+    for (i = 0; num > 0; i++)
         if (data[i] == '\n')
             num--;
-    sscanf(&data[i], "%s\n", word);
+    swscanf(&data[i], L"%s\n", word);
 }
